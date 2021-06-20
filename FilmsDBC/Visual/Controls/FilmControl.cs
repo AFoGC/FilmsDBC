afa76d876dc2765b8ffda16fd3a78d3879db3d2a
Andrey Le <sirkoandrew84@gmail.com>
2021-06-20 16:06:32 +0300

    Не большие изменение в FilmControl

--- FilmsDBC/Visual/Controls/FilmControl.cs
+++ FilmsDBC/Visual/Controls/FilmControl.cs
@@ -1,5 +1,5 @@
 ﻿using FilmsDBC.CinemaDataTypes;
-using FilmsDBC.StaticFilmClasses;
+using FilmsDBC.Visual.StaticVisualClasses;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,11 +24,11 @@ namespace FilmsDBC.Visual.Controls
 			this.textBox_id.Text = filmInfo.ID.ToString();
 			this.textBox_name.Text = filmInfo.name;
 			this.textBox_genre.Text = filmInfo.genre;
-			this.textBox_realiseYear.Text = FormatText.formatToText(filmInfo.realiseYear);
+			this.textBox_realiseYear.Text = Film.formatToString(filmInfo.realiseYear);
 			this.checkBox_watched.Checked = filmInfo.watched;
-			this.textBox_watchDate.Text = FormatText.formatToText(filmInfo.dateOfWatch);
-			this.textBox_mark.Text = FormatText.formatToText(filmInfo.mark);
-			this.textBox_countOfviews.Text = FormatText.formatToText(filmInfo.countOfviews);
+			this.textBox_watchDate.Text = Film.formatToString(filmInfo.dateOfWatch);
+			this.textBox_mark.Text = VisualHelper.markToText(Film.formatToString(filmInfo.mark));
+			this.textBox_countOfviews.Text = Film.formatToString(filmInfo.countOfviews);
 		}
 	}
 }
---
+++ FilmsDBC/Visual/StaticVisualClasses/VisualHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmsDBC.Visual.StaticVisualClasses
+{
+    class VisualHelper
+    {
+        public static String markToText(String mark)
+        {
+            switch (mark)
+            {
+                case "":
+                    return mark;
+
+                case "6":
+                    return mark + "/6";
+
+                default:
+                    return mark + "/5";
+            }
+        }
+    }
+}
