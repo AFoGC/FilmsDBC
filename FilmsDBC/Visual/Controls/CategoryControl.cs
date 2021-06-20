b5ee4f5eb7c089c4aea0f87f7068d2ce4d4cc92d
Andrey Le <sirkoandrew84@gmail.com>
2021-06-20 23:21:38 +0300

    Создал CategotyControl, SerieControl, но сделал только конструкторы.

---
+++ FilmsDBC/Visual/Controls/CategoryControl.cs
@@ -0,0 +1,25 @@
+﻿using FilmsDBC.CinemaDataTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmsDBC.Visual.Controls
+{
+    public partial class CategoryControl : UserControl
+    {
+        Category categoryInfo = null;
+        public CategoryControl(Category category)
+        {
+            InitializeComponent();
+            this.categoryInfo = category;
+
+
+        }
+    }
+}
---
+++ FilmsDBC/Visual/Controls/SerieControl.cs
@@ -0,0 +1,27 @@
+﻿using FilmsDBC.CinemaDataTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmsDBC.Visual.Controls
+{
+    public partial class SerieControl : UserControl
+    {
+        Serie serieInfo = null;
+        Film filmInfo = null;
+        public SerieControl(Film film, Serie serie)
+        {
+            InitializeComponent();
+            this.serieInfo = serie;
+            this.filmInfo = film;
+
+
+        }
+    }
+}
