2b82e0ae8d44df0d915294115bd367623b9bfe99
Andrey Le <sirkoandrew84@gmail.com>
2021-07-11 15:35:50 +0300

    Эксприментировал с наследованием абстрактных контролов, кторый абстрактно реализует интерфейс, и оставил небольшие наработки на будущие

---
+++ FilmsDBC/Visual/Controls/Interface/AElementControl.cs
@@ -0,0 +1,25 @@
+﻿using FilmsDBC.CinemaDataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmsDBC.Visual.Controls
+{
+    public abstract class AElementControl : UserControl, IControls
+    {
+        internal Film filmInfo = null;
+        public Film FilmInfo
+        {
+            get { return filmInfo; }
+        }
+
+        public abstract void ChangeColorToSearch();
+        public abstract void ChangeColorToSelect();
+
+        //public abstract void ChangeColorToSearch();
+        //public abstract void ChangeColorToSelect();
+    }
+}
---
+++ FilmsDBC/Visual/Controls/Interface/IControls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmsDBC.Visual.Controls
+{
+    public interface IControls
+    {
+        void ChangeColorToSelect();
+        void ChangeColorToSearch();
+    }
+}
