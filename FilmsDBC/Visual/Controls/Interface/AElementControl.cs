1f2ab79926925d429a8a0b0120ab77edfa330989
Andrey Le <sirkoandrew84@gmail.com>
2021-07-12 22:03:47 +0300

    Написал поставщик описания для абстрактного элементу управления AElementControl. Теперь можно применять абстрактные контролы

--- FilmsDBC/Visual/Controls/Interface/AElementControl.cs
+++ FilmsDBC/Visual/Controls/Interface/AElementControl.cs
@@ -1,6 +1,7 @@
 ﻿using FilmsDBC.CinemaDataTypes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Forms;
 
 namespace FilmsDBC.Visual.Controls
 {
+    [TypeDescriptionProvider(typeof(AbstractControlDescriptionProvider<AElementControl, UserControl>))]
     public abstract class AElementControl : UserControl, IControls
     {
         internal Film filmInfo = null;
