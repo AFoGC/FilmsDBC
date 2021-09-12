2f26a2fd37a19e3e45f1fceaa2ca6fbb202b9a20
Andrey Le <sirkoandrew84@gmail.com>
2021-09-13 00:08:45 +0300

    работал над настройкой директорий и недоработал.

--- FilmsDBC/Visual/Forms/GlobalControls/SettingsControlElements/DirectoryPathSettingControl.cs
+++ FilmsDBC/Visual/Forms/GlobalControls/SettingsControlElements/DirectoryPathSettingControl.cs
@@ -11,35 +11,76 @@ using System.Windows.Forms;
 
 namespace FilmsDBC.Visual.Forms.GlobalControls.SettingsControlElements
 {
-    public partial class DirectoryPathSettingControl : UserControl, ISettingsControls
-    {
-        public DirectoryPathSettingControl()
-        {
-            InitializeComponent();
-            clickButton_changeDirectory.SetDefaultButtonBlink();
-        }
+	public partial class DirectoryPathSettingControl : UserControl, ISettingsControls
+	{
+		public DirectoryPathSettingControl()
+		{
+			InitializeComponent();
+			clickButton_changeDirectory.SetDefaultButtonBlink();
+		}
 
-        public void GetSetting()
-        {
-            MainInformation.Settings.DirectoryPath = textBox_filePath.Text;
-        }
+		public void GetSetting()
+		{
+			MainInformation.Settings.DirectoryPath = textBox_filePath.Text;
+		}
 
-        public void RefreshControl()
-        {
-            textBox_filePath.Text = MainInformation.Settings.DirectoryPath;
-        }
+		public void RefreshControl()
+		{
+			textBox_filePath.Text = MainInformation.Settings.DirectoryPath;
+		}
 
-        private void clickButton_changeDirectory_Click(object sender, EventArgs e)
-        {
-            using (var fbd = new FolderBrowserDialog())
-            {
-                DialogResult result = fbd.ShowDialog();
+		private void clickButton_changeDirectory_Click(object sender, EventArgs e)
+		{
+			using (var fbd = new FolderBrowserDialog())
+			{
+				DialogResult result = fbd.ShowDialog();
 
-                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-                {
-                    textBox_filePath.Text = fbd.SelectedPath;
-                }
-            }
-        }
-    }
+				if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{
+					textBox_filePath.Text = fbd.SelectedPath;
+				}
+				String filename = "Films.fdbc";
+
+				try
+				{
+					//
+					// Доделать всю эту хуйню с работой и прверкой изминения директорий.
+					//
+
+
+
+					//if (!checkForFilename(filename, Directory.GetFiles(fbd.SelectedPath)))
+					String filmPathName = fbd.SelectedPath + "\\" + filename;
+					if (File.Exists(filmPathName))
+					{
+						//File.Create(filmPathName);
+						//MainInformation.tableCollection.tableFilePath = filmPathName;
+
+						using (FileStream sr = File.Create(filmPathName))
+						{
+							String strA = MainInformation.tableCollection.GetVoidTableCollection();
+							byte[] info = new UTF8Encoding(true).GetBytes(strA);
+							sr.Write(info, 0, info.Length);
+						}
+						MainInformation.LoadTables();
+					}
+				}
+                finally { }
+			}
+		}
+
+
+
+		private bool checkForFilename(String filename, String[] files)
+		{
+			foreach (String file in files)
+			{
+				if (file == filename)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
 }
